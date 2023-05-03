49eadd7887cde9f6287c01f8e2ac4256d49b5399
oalshokri <janataanfor@gmail.com>
2023-05-03 10:47:54 +0300

    - bank account

--- src/Ch3/Account.java
+++ src/Ch3/Account.java
@@ -1,31 +1,49 @@
 package Ch3;
 
 public class Account {
-    private double balance = 0;
+    private double balance;
+    private int accountNumber;
 
-    void deposit(double amount){
-        balance = balance + amount;
+    public Account(int accountNumber,double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
     }
-    boolean withdraw(double amount){
-        if(amount > balance){
-            return false;
+
+
+    double deposit(double amount){
+        balance = balance + amount;
+        return  amount;
+    }
+    double withdraw(double amount){
+        if(amount > balance || amount == 0){
+            return 0;
         }
         balance = balance - amount;
 
-        return  true;
+        return  amount;
     }
     double getBalance(){
         return  balance;
+    }
+    int getAccountNumber(){
+        return  accountNumber;
     }
 }
 
 class MyBank{
     public static void main(String[] args) {
-        Account osamaAccount = new Account();
-        osamaAccount.deposit(100);
-        boolean result = osamaAccount.withdraw(300);
-       if(result){
-           System.out.println("withdraw done");
+        Account myAccount = new Account(12,300);
+        System.out.println("Account Number: "+myAccount.getAccountNumber());
+        System.out.println("your balance: " +myAccount.getBalance());
+
+        double d = myAccount.deposit(100);
+        System.out.println("deposit done, amount:"+ d);
+        System.out.println("your balance: " +myAccount.getBalance());
+
+        double result = myAccount.withdraw(300);
+       if(result != 0){
+           System.out.println("withdraw done, amount: "+result);
+           System.out.println("your balance: " +myAccount.getBalance());
        }else{
            System.out.println("balance not enough");
        }
